f5d3762a10c6edd931f5a92b5adb3f85c6c8303b
Louis <yuluyi1991@gmail.com>
2016-01-13 11:35:39 +0800

    article view

--- src/containers/Library/index.js
+++ src/containers/Library/index.js
@@ -1,7 +1,9 @@
 import External from './External/External';
 import Internal from './Internal/Internal';
+import ArticleView from '#/components/ArticleView';
 
 export default {
   External,
-  Internal
+  Internal,
+  ArticleView
 }
