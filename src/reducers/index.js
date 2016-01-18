fce76c1cfda2405d15e007c6e46283972b378c49
Louis <yuluyi1991@gmail.com>
2016-01-18 18:43:58 +0800

    add auto generated reducers for redux-rest

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,8 +4,18 @@ import { routerStateReducer } from 'redux-router';
 import welcome from './welcome';
 import user from './user';
 import organizations from './organizations';
+import Flux from '#/utils/redux-rest';
+import { path } from '#/utils/params';
+
+const myAPI = {
+  categories: ({orgId, productId} = path()) => `/organizations/${orgId}/products/${productId}/categories`,
+  articles: ({orgId, productId, categoryId} = path()) => `/organizations/${orgId}/products/${productId}/categories/${categoryId}/articles`
+}
+
+export const flux = new Flux(myAPI);
 
 export default combineReducers({
+  ...flux.reducers,
   router: routerStateReducer,
   welcome,
   user,
@@ -13,6 +23,5 @@ export default combineReducers({
   productions: (state = {}, action) => state,
   libraries: (state = {}, action) => state,
   drafts: (state = {}, action) => state,
-  distributions: (state = {}, action) => state,
-  configs: (state = {}, action) => state,
+  configs: (state = {}, action) => state
 });
